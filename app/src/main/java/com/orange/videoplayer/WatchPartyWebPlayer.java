4f98a808d9f23edb19257705206aaa1b860a4978
ALAAWF2 <wafaiealaa@gmail.com>
2026-08-21 15:11:06 +0300

    Update hosted web player URL to root GitHub Pages

--- app/src/main/java/com/orange/videoplayer/WatchPartyWebPlayer.java
+++ app/src/main/java/com/orange/videoplayer/WatchPartyWebPlayer.java
@@ -6,7 +6,7 @@ import java.net.URLEncoder;
 public class WatchPartyWebPlayer {
 
     // Live hosted GitHub Pages Web Player for iOS Safari & Web
-    private static final String HOSTED_WEB_PLAYER_URL = "https://alaawf2.github.io/videoshare/web/";
+    private static final String HOSTED_WEB_PLAYER_URL = "https://alaawf2.github.io/videoshare/";
 
     public static String getShareableWebUrl(String roomId, String videoUrl, String videoTitle) {
         if (roomId == null) roomId = "WP-1001";
